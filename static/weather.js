2db02c363a5076778852b3c50431bfccb750f828
Seth Brummund <swbrummund@gmail.com>
2019-11-19 18:14:16 -0600

    stuff

--- static/weather.js
+++ static/weather.js
@@ -122,3 +122,22 @@ var colorPop = ['#ffffff', '#ccd9ff', , '#b3c6ff', '#99b3ff', '#668cff', '#4d79f
     L.control.layers(baseMaps,overlayMaps, {
       collapsed: false
     }).addTo(myMap);
+
+    function highlightFeature(e) {
+      var layer = e.target;
+  
+      layer.setStyle({
+          weight: 5,
+          color: '#666',
+          dashArray: '',
+          fillOpacity: 0.7
+      });
+  
+      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+          layer.bringToFront();
+      }
+  }
+
+  function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
